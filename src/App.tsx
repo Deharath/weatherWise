269698e9c50e25b1b590cf0358e244813a5d1ccb
Gracjan <gracjando@gmail.com>
2023-05-15 14:20:09 +0200

    create basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
-    <div className=' bg-slate-600 h-screen w-full'>
-      <Header />
-    </div>
+    <>
+      <MainContainer />
+    </>
   );
 };
 
@@ -40,7 +40,7 @@ const Header = () => {
 
   return (
     <header className="p-6 mb-8 ">
-      <h1 className="text-3xl font-bold mb-4 text-slate-50">Weather App</h1>
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Weather App</h1>
       <div className="search-bar">
         <input
           type="text"
@@ -52,6 +52,80 @@ const Header = () => {
         <button onClick={handleSearch} className="bg-blue-500 text-white px-4 py-2 rounded">Search</button>
       </div>
     </header>
+  );
+};
+
+const MainContainer = () => {
+  return (
+
+    <div className="flex flex-col items-center min-h-screen bg-gray-100 ">
+      <Header />
+      <div className="flex flex-row space-y-4 md:space-y-0 md:space-x-4 bg-white p-4 rounded shadow-lg w-4/5">
+        <div className="flex flex-col space-y-4 w-4/5">
+          <div className="flex flex-col space-y-2">
+            <h1 className="font-bold text-2xl">City Name</h1>
+            <p>Chance of Rain: 30%</p>
+            Weather Icon
+            <p>Current Temperature: 20&deg;C</p>
+          </div>
+          <div className="flex flex-row space-x-2 justify-around">
+            <div className="flex flex-col items-center space-y-2 w-1/5">
+              <p>1 am</p>
+              Weather Icon
+              <p>Temperature</p>
+            </div>
+            <div className="flex flex-col items-center space-y-2 w-1/5">
+              <p>1 am</p>
+              Weather Icon
+              <p>Temperature</p>
+            </div>
+            <div className="flex flex-col items-center space-y-2 w-1/5">
+              <p>1 am</p>
+              Weather Icon
+              <p>Temperature</p>
+            </div>
+            <div className="flex flex-col items-center space-y-2 w-1/5">
+              <p>1 am</p>
+              Weather Icon
+              <p>Temperature</p>
+            </div>
+            <div className="flex flex-col items-center space-y-2 w-1/5">
+              <p>1 am</p>
+              Weather Icon
+              <p>Temperature</p>
+            </div>
+            <div className="flex flex-col items-center space-y-2 w-1/5">
+              <p>1 am</p>
+              Weather Icon
+              <p>Temperature</p>
+            </div>
+          </div>
+          <div className="flex flex-row space-x-4 justify-around">
+            <div>
+              <p>Feeling Temperature: 20&deg;C</p>
+            </div>
+            <div>
+              <p>Wind Speed: 10km/h</p>
+            </div>
+            <div>
+              <p>Chance of Rain: 30%</p>
+            </div>
+            <div>
+              <p>Pressure: 1013hPa</p>
+            </div>
+          </div>
+
+        </div>
+        <div className='flex-col h-full flex justify-center'>
+          <div className="flex flex-row items-center justify-around space-y-2 w-full">
+            <p>1 am</p>
+            Weather Icon
+            <p>Temperature</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   );
 };
 
