325e4b97d0b4710e45782b2b43ffd93710914125
Gracjan <gracjando@gmail.com>
2023-05-16 12:41:00 +0200

    further app design polish

--- src/App.tsx
+++ src/App.tsx
@@ -56,20 +56,22 @@ const Header = () => {
 const MainContainer = () => {
   return (
 
-    <div className="flex flex-col items-center h-full justify-center">
-      <div className="flex flex-col bg-white p-4 rounded shadow-lg h-3/4 w-3/4">
+    <div className="flex flex-col items-center h-full justify-center bg-gradient-to-tr from-blue-900 via-blue-600 to-blue-400 font-sans">
+      <div className="flex flex-col bg-gray-800 p-4 rounded shadow-lg h-3/4 w-3/4 opacity-95">
         <Header />
-        <div className='flex flex-row p-3 h-full'>
-          <div className="flex flex-col space-y-8 w-4/6">
+        <div className='flex flex-row p-3 h-full text-white'>
+          <div className="flex flex-col space-y-3 w-4/6">
 
-            <div className="flex flex-col space-y-2 h-1/3">
-              <h1 className="font-bold text-2xl ">City Name</h1>
+            <div className="flex flex-col space-y-2 h-1/3 p-3">
+              <div className='flex flex-row justify-between'>
+                <h1 className="font-bold text-2xl">City Name</h1>
+                <div>Weather Icon</div>
+              </div>
               <p>Chance of Rain: 30%</p>
-              Weather Icon
               <p>Current Temperature: 20&deg;C</p>
             </div>
 
-            <div className="flex flex-row justify-around items-center rounded-lg p-3 bg-teal-400 h-1/3">
+            <div className="flex flex-row justify-around items-center rounded-lg p-3 bg-gray-600 h-1/3">
               Today's forecast
               <div className="flex flex-col items-center space-y-2  p-2 w-1/5 text-center">
                 <p>1 am</p>
@@ -103,27 +105,33 @@ const MainContainer = () => {
               </div>
             </div>
 
-            <div className="flex flex-col space-x-4 justify-around items-center rounded-lg p-3 bg-teal-400 h-1/3">
+            <div className="flex flex-col space-x-4 justify-around items-center rounded-lg p-3 bg-gray-600 h-1/3">
+              <div className=' self-start flex'>Air Conditions</div>
               <div className='flex flex-row w-full'>
-                <div className='w-1/2 flex'>
-                  <p>Feeling Temperature: 20&deg;C</p>
+                <div className='w-1/2 flex flex-col'>
+                  <p>Feeling Temperature</p>
+                  <p>20&deg;C</p>
                 </div>
-                <div className='w-1/2 flex'>
-                  <p>Wind Speed: 10km/h</p>
+                <div className='w-1/2 flex flex-col'>
+                  <p>Wind Speed</p>
+                  <p>10km/h</p>
                 </div>
               </div>
               <div className='flex flex-row w-full'>
-                <div className='w-1/2 flex'>
-                  <p>Chance of Rain: 30%</p>
+                <div className='w-1/2 flex flex-col'>
+                  <p>Chance of Rain</p>
+                  <p>30%</p>
                 </div>
-                <div className='w-1/2 flex'>
-                  <p>Pressure: 1013hPa</p>
+                <div className='w-1/2 flex flex-col'>
+                  <p>Pressure</p>
+                  <p>1013hPa</p>
                 </div>
               </div>
             </div>
 
           </div>
-          <div className='flex-col h-full flex justify-evenly w-2/6 ml-3'>
+          <div className='flex-col h-full flex justify-around w-2/6 ml-3 rounded-lg p-3 bg-gray-600'>
+            <div className=' self-start flex'>7 Day Forecast</div>
             <div className="flex flex-row items-center justify-around w-full">
               <p>Saturday</p>
               Weather Icon
