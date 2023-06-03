c5339416392e1fb44d0b21f8fed8c120993f7ad4
Deharath <gracjando@gmail.com>
2023-06-03 13:31:57 +0200

    do final polish for main functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -110,8 +110,9 @@ const App: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center h-full justify-center bg-gradient-to-tr from-blue-900 via-blue-600 to-blue-400 font-sans p-5 overflow-hidden">
-      <div className={`flex flex-col bg-gray-800 rounded shadow-lg bg-opacity-80 overflow-hidden ${data != null && "md:w-1/2"} w-full max-w-4xl md:p-5 p-4 shadow shadow-gray-300`}>
-        <div className="flex md:flex-row flex-col h-full text-white ">
+      <h1 className=" font-bold text-5xl mb-5 text-white">WeatherWise</h1>
+      <div className={`flex flex-col bg-gray-800 rounded shadow-lg bg-opacity-70 overflow-hidden ${data != null && "md:w-full"} max-w-4xl md:p-5 p-4 shadow shadow-gray-800`}>
+        <div className="flex md:flex-row flex-col h-full text-white">
           <div className={`flex flex-col space-y-3 w-full h-full ${data != null && "md:mr-3"}`}>
             <div className="flex flex-row w-full text-black">
               <div className="relative w-full">
@@ -128,22 +129,21 @@ const App: React.FC = () => {
                   onKeyDown={handleKeyDown}
                   className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter city name" />
                 <button onClick={handleSearch} className="flex justify-center items-center w-3/12 md:w-1/5 text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                {!loading ?
-                  ("Search")
-                  :
-                  (
-                    <svg aria-hidden="true" className="h-5 w-5 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-                    </svg>
-                  )}
-                  </button>
+                  {!loading ?
+                    ("Search")
+                    :
+                    (
+                      <svg aria-hidden="true" className="h-5 w-5 text-gray-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                      </svg>
+                    )}
+                </button>
               </div>
             </div>
-
-            {data != null &&
-              <>
-                <div className="flex flex-row p-3 justify-between ">
+            <div className={`flex flex-row p-3 ${data !== null ?  "justify-between" : "justify-center"}`}>
+              {data !== null ? (
+                <>
                   <div className="flex flex-col">
                     <h1 className="font-bold text-2xl mb-3">{city}</h1>
                     <p>Humidity: {data?.current.humidity}%</p>
@@ -154,10 +154,18 @@ const App: React.FC = () => {
                     alt={data?.current.weather[0].description}
                     className="  object-cover"
                   />
-                </div>
-                <div className="flex-1 flex-col space-y-3 w-full overflow-auto h-1/2">
+                </>):(
+              <div className="flex flex-col justify-center items-center">
+                <div className="text-xl mb-3 text-center space-y-3"><p>Welcome to WeatherWise, your personal weather station.</p> <p>Discover current conditions, see hourly trends, and plan your week with a seven-day forecast.</p> <p>Start by entering a city name in the search bar.</p></div>
+              </div>
+              )}
+
+            </div>
+            {data != null &&
+              <>
+                <div className="flex-1 flex-col space-y-3 w-full overflow-auto h-1/2 scroll-smooth">
                   <div className="flex flex-col rounded-lg p-3 bg-gray-600 bg-opacity-80 justify-evenly w-full">
-                    <h2 className="mb-2 font-semibold">Today"s forecast</h2>
+                    <h2 className="mb-2 font-semibold">Today's forecast</h2>
                     <div className="flex flex-row items-center h-full overflow-x-auto w-full">
                       <HourlyForecast data={data} />
                     </div>
