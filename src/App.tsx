02f97acc5268280caa2c32541988adb47687733e
Deharath <gracjando@gmail.com>
2023-07-27 09:49:33 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -115,7 +115,7 @@ const App: React.FC = () => {
         <div className="flex md:flex-row flex-col h-full text-white">
           <div className={`flex flex-col space-y-3 w-full h-full ${data != null && "md:mr-3"}`}>
             <div className="flex flex-row w-full text-black">
-              <div className="relative w-full">
+              <div className="relative w-full flex">
                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                   <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
@@ -155,8 +155,8 @@ const App: React.FC = () => {
                     className="  object-cover"
                   />
                 </>):(
-              <div className="flex flex-col justify-center items-center">
-                <div className="text-xl mb-3 text-center space-y-3"><p>Welcome to WeatherWise, your personal weather station.</p> <p>Discover current conditions, see hourly trends, and plan your week with a seven-day forecast.</p> <p>Start by entering a city name in the search bar.</p></div>
+              <div className="flex flex-col justify-center">
+                <div className="text-lg mb-3 text-left space-y-2"><p><p>Welcome to WeatherWise, your personal weather station.</p> <p>Discover current conditions, see hourly trends, and plan your week with a seven-day forecast.</p></p> <p className="text-gray-400 font-semibold">Start by entering a city name in the search bar.</p></div>
               </div>
               )}
 
